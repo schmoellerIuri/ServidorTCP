1839956e84e886204ca4d69ae696598a081cc01d
Iuri <iurischmoeller@hotmail.com>
2023-08-30 16:29:52 -0300

    Ajustes para multithreading

--- Program.cs
+++ Program.cs
@@ -14,61 +14,83 @@ var servidor = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolT
 servidor.Bind(ipEndPoint);
 servidor.Listen(10);
 
-var handler = await servidor.AcceptAsync();
-
 while (true)
 {
-    //recebe o tamanho em bytes da mensagem
-    var sizeInBytes = new byte[4];
-    _ = await handler.ReceiveAsync(sizeInBytes, SocketFlags.None);
-    var size = BitConverter.ToInt32(sizeInBytes, 0);
+    var handler = await servidor.AcceptAsync();
+    var thread = new Thread(DoWork);
+    thread.Start(handler);
+}
 
-    //recebe a mensagem
-    var buffer = new byte[size];
-    var qtdBytesRequest = await handler.ReceiveAsync(buffer, SocketFlags.None);
-    var request = Encoding.UTF8.GetString(buffer, 0, qtdBytesRequest);
 
-    if(String.IsNullOrEmpty(request))
-        continue;
-
-    string operacao = request[..5];
-    byte[] retornoAoCliente = Array.Empty<byte>();
-
-    try
+void DoWork(object handler)
+{
+    var _handler = (Socket) handler;
+        
+    while (true)
     {
-        switch (operacao)
+        //recebe o tamanho em bytes da mensagem
+        var sizeInBytes = new byte[4];
+        try
         {
-            case "|<C>|":
-                TarefasManager.CriarTarefa(DecoderTarefas.DecodeTarefa(request[5..], false));
-                retornoAoCliente = Encoding.UTF8.GetBytes("Tarefa criada com sucesso!");
-                break;
-            case "|<R>|":
-                var idRequisitado = int.Parse(request[5..]);
-                retornoAoCliente = idRequisitado == 0 ? EncoderTarefas.EncodeListaDeTarefas(TarefasManager.LerTarefas()) : Encoding.UTF8.GetBytes(EncoderTarefas.EncodeTarefa(TarefasManager.LerTarefaPorId(idRequisitado)));
-                break;
-            case "|<U>|":
-                TarefasManager.AtualizarTarefa(DecoderTarefas.DecodeTarefa(request[5..], true));
-                retornoAoCliente = Encoding.UTF8.GetBytes("Tarefa atualizada com sucesso!");
-                break;
-            case "|<D>|":
-                TarefasManager.RemoverTarefa(int.Parse(request[5..]));
-                retornoAoCliente = Encoding.UTF8.GetBytes("Tarefa removida com sucesso!");
-                break;
-            default:
-                retornoAoCliente = Encoding.UTF8.GetBytes("Operação inválida");
-                break;
+            _ = _handler.Receive(sizeInBytes, SocketFlags.None);
+        }
+        catch
+        { break; }
+
+        var size = BitConverter.ToInt32(sizeInBytes, 0);
+
+        //recebe a mensagem
+        var buffer = new byte[size];
+        var qtdBytesRequest = _handler.Receive(buffer, SocketFlags.None);
+        var request = Encoding.UTF8.GetString(buffer, 0, qtdBytesRequest);
+
+        if (String.IsNullOrEmpty(request))
+            continue;
+
+        string operacao = request[..5];
+        byte[] retornoAoCliente = Array.Empty<byte>();
+
+        if (string.Equals(operacao, "|<E>|")) 
+            break;
+        
+
+        try
+        {
+            switch (operacao)
+            {
+                case "|<C>|":
+                    TarefasManager.CriarTarefa(DecoderTarefas.DecodeTarefa(request[5..], false));
+                    retornoAoCliente = Encoding.UTF8.GetBytes("Tarefa criada com sucesso!");
+                    break;
+                case "|<R>|":
+                    var idRequisitado = int.Parse(request[5..]);
+                    retornoAoCliente = idRequisitado == 0 ? EncoderTarefas.EncodeListaDeTarefas(TarefasManager.LerTarefas()) : Encoding.UTF8.GetBytes(EncoderTarefas.EncodeTarefa(TarefasManager.LerTarefaPorId(idRequisitado)));
+                    break;
+                case "|<U>|":
+                    TarefasManager.AtualizarTarefa(DecoderTarefas.DecodeTarefa(request[5..], true));
+                    retornoAoCliente = Encoding.UTF8.GetBytes("Tarefa atualizada com sucesso!");
+                    break;
+                case "|<D>|":
+                    TarefasManager.RemoverTarefa(int.Parse(request[5..]));
+                    retornoAoCliente = Encoding.UTF8.GetBytes("Tarefa removida com sucesso!");
+                    break;
+                default:
+                    retornoAoCliente = Encoding.UTF8.GetBytes("Operação inválida");
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            retornoAoCliente = Encoding.UTF8.GetBytes($"Erro ao executar operação: {ex.Message}");
+
+        }
+        finally
+        {
+            var sizeResponse = retornoAoCliente.Length;
+            _handler.Send(BitConverter.GetBytes(sizeResponse), SocketFlags.None);
+            _handler.Send(retornoAoCliente, SocketFlags.None);
         }
     }
-    catch (Exception ex)
-    {
-        retornoAoCliente = Encoding.UTF8.GetBytes($"Erro ao executar operação: {ex.Message}");
 
-    }
-    finally
-    {
-        var sizeResponse = retornoAoCliente.Length;
-        await handler.SendAsync(BitConverter.GetBytes(sizeResponse), SocketFlags.None);
-        await handler.SendAsync(retornoAoCliente, SocketFlags.None);
-    }
-
+    _handler.Close();
 }
